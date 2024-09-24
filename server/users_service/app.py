a57e500b82129f5c512ec625f7e05c4c99f7458a
Artem <artem.chizg@gmail.com>
2024-09-24 15:16:51 +0300

    Add endpoints for users (CRUD)

--- server/users_service/app.py
+++ server/users_service/app.py
@@ -12,9 +12,59 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
 
-@app.route("/", methods=["GET"])
-def test():
-    return jsonify({ "user": "Success!" })
+@app.route("/users", methods=["GET"])
+def get_users():
+    users = User.query.all()
+    users_list = [{
+        "id": user.id,
+        "username": user.username
+    } for user in users]
+    return jsonify(users_list)
+
+@app.route("/users/<int:id>", methods=["GET"])
+def get_user(id: int):
+    user = User.query.get(id)
+    if not user: return jsonify({ "error": "User not found" }), 404
+    
+    return jsonify({
+        "id": user.id,
+        "username": user.username,
+        "password_hash": user.password_hash
+    }), 200
+
+@app.route("users/<string:username>", methods=["GET"])
+def get_user(username: str):
+    user = User.query.filter_by(username=username).first()
+    if not user: return jsonify({ "error": "User not found" }), 404
+    
+    return jsonify({
+        "id": user.id,
+        "username": user.username,
+        "password_hash": user.password_hash
+    }), 200
+
+@app.route("users/<int:id>", methods=["DELETE"])
+def delete_user(id: int):
+    user = User.query.get(id)
+    if not user: return jsonify({ "error": "User not found" }), 404
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({ "message": "User deleted succesfully" }), 200
+
+@app.route("users/<int:id>", methods=["PUT"])
+def update_user(id: int):
+    user = User.query.get(id)
+    if not user: return jsonify({ "error": "User not found" }), 404
+    data = request.get_json()
+    if "username" in data: user.username = data["username"]
+    if "password_hash" in data: user.password_hash = data["password_hash"]
+    
+    try:
+        db.session.commit()
+        return jsonify({" message": "User updated successfully" }), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({ "error": str(e) }), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
